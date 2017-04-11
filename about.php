eaa0242cfe6befc493057b19c8923e19b89759b6
guigrande <guiaugus@br.ibm.com>
2017-04-11 15:40:00 -0300

    aboutfied

--- about.php
+++ about.php
@@ -8,10 +8,9 @@ include_once('main.php');
 <div class="box_top_div"><a href="#">Start</a> &gt; About</div>
 <div class="box_body_div">
 
-<p class="center_p"><a href="Open Source Code"</a>
-<p class="center_p"><a href="Original Version:"</a>
+<p class="center_p"><a>Open Source Code</a></p> &gt;
+<p class="center_p"><a>Original Code:</a></p> &gt;
 <p class="center_p"><a href="<?php echo global_project_website; ?>" target="_blank">Visit website</a></p>
-<p class="center_p"><a href="Adaptaded code by: Gustavo A./Gustavo M./Guilherme G. "</a>
-
+<p class="center_p"><a>Adapted and Developed by: Gustavo A./ Gustavo M./ Guilherme G.</a></p> &gt;
 
 </div></div>
